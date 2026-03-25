e7f859cd47924829bfd0956f11874ec19165ebcc
aashish <ritikkamboj4314@gmail.com>
2026-03-25 18:55:19 +0530

    created connection from client to web socket server by making a context

---
+++ src/context/socketContext.tsx
@@ -0,0 +1,20 @@
+import React, { createContext } from "react";
+import { io, Socket } from "socket.io-client";
+
+const WS_Server = "http://localhost:5500";
+
+const socket: Socket = io(WS_Server);
+
+const SocketContext = createContext<Socket | null>(null);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const SocketProvider: React.FC<Props> = ({ children }) => {
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
+
+export default SocketProvider;
