8c7e0f7fde3f86266eff3ddaf109f0843c02ffa1
aashish <ritikkamboj4314@gmail.com>
2026-03-25 15:59:27 +0530

    initialize the client with tailwind css and daisy Ui configured

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="font-bold underline bg-amber-400 px-10">
+      <p>Hello World</p>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
