22902e9ed5517451f7b05d997c5bc2548a0795b7
aashish <ritikkamboj4314@gmail.com>
2026-03-30 17:03:47 +0530

    emit event created when user joined the room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,8 +1,19 @@
 import type React from "react";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { SocketContext } from "../context/socketContext";
 
 const Room: React.FC = () => {
   const { id } = useParams();
+  const {socket} =  useContext(SocketContext)
+
+  useEffect(()=>{
+
+    // this message we sent when the new user create the room and joined and also
+    // when the some other user is entering  in the room which is laready created 
+    socket.emit("joined-room", {roomId : id})
+
+  },[])
   return <div>room is : {id}</div>;
 };
 export default Room;
